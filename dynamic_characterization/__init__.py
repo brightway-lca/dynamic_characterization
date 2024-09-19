b132f6bbcc0d7e9897d503acb9c13deba708f311
TimoDiepers <timo.diepers@gmail.com>
2024-09-19 11:06:27 +0200

    fix __init__.py

--- dynamic_characterization/__init__.py
+++ dynamic_characterization/__init__.py
@@ -5,7 +5,7 @@ Collection of dynamic characterization functions for life cycle inventories with
 __all__ = (
     "__version__",
     "dynamic_characterization.characterize",
-    "dynamic_characterization.create_characterization_function_dict_from_method"
+    "dynamic_characterization.create_characterization_function_dict_from_method",
     "original_temporalis_functions",
     "ipcc_ar6",
     # Add functions and variables you want exposed in `dynamic_characterization.` namespace here
